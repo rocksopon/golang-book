1e4c847578151c471740efea25b874a4473a9546
rock <rocksopon@gmail.com>
2018-02-08 10:35:59 +0700

    Farenheit to Celsius Program

--- chapter4-2/main.go
+++ chapter4-2/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Enter a number: ")
+	fmt.Print("Enter a Farenheit: ")
 	var input float64
 	fmt.Scanf("%f", &input)
-	output := input * 2
-	fmt.Println(output)
+	output := ((input - 32) * 5/9)
+	fmt.Printf("%.2f Farenheit = %.2f Celsius", input, output)
 }
