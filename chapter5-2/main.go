dcdaea3240d9a9b29d060e0fb27902b5feef7212
rock <rocksopon@gmail.com>
2018-02-13 10:09:26 +0700

    Add chapter5-2

---
+++ chapter5-2/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	for number := 1 ; number <=100 ; number++ {
+		if number%15 == 0 {
+			fmt.Println("FizzBuzz")
+		}else if number%3 == 0 {
+			fmt.Println("Fizz")
+		}else if number%5 == 0 {
+			fmt.Println("Buzz")
+		}else {
+			fmt.Println(number)
+		}
+	}
+
+}
