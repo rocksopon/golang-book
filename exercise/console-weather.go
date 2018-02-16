25ad847492162b102583acc6cf75ae56843a59d3
rock <rocksopon@gmail.com>
2018-02-16 10:15:58 +0700

    Add exercise

---
+++ exercise/console-weather.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"strconv"
+	"fmt"
+)
+
+func main() {
+	weatherCelcius(25, "Bangkok few cloud")
+}
+
+func weatherCelcius(celcius int, description string) {
+	two :=  ` _
+ _|
+|_`
+	
+	five := ` _
+|_
+ _|`
+ 
+	celciusStr := strconv.Itoa(celcius)
+	for _, value := range celciusStr {
+		valueStr := string(value)
+		if valueStr == "2" {
+			fmt.Println(two)
+		}else if  valueStr == "5" {
+			fmt.Print(five)
+		}
+	}
+	fmt.Println(description)
+}
+
